954c4743f81f3e5fee8b8b125fb35a0c1a6aa797
Tasos Derisiotis <50984242+Eengineer1@users.noreply.github.com>
2023-05-17 12:29:57 +0200

    fix: Switched to file imports

--- src/utils.ts
+++ src/utils.ts
@@ -34,8 +34,8 @@ import {
     MsgCreateDidDocPayload,
     MsgDeactivateDidDocPayload,
 } from "@cheqd/ts-proto/cheqd/did/v2/index.js"
-import { DIDModule } from "./modules/did"
-import { MsgCreateResourcePayload } from "@cheqd/ts-proto/cheqd/resource/v2/tx.js"
+import { DIDModule } from "./modules/did.js"
+import { MsgCreateResourcePayload } from "@cheqd/ts-proto/cheqd/resource/v2/index.js"
 
 export type TImportableEd25519Key = {
     publicKeyHex: string
